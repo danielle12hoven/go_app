41709407245b28f691c294b6041050cfcba37512
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-20 13:53:37 -0500

    commit issue

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,4 @@ Rails.application.routes.draw do
 
 
   get'/edit', to: 'home#edit', as: :edit
-
-
-
-=======
-  resources :home
-  root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 37d6659479d2dcab4aea7fa2430354118a18da10
 end
